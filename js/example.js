58dcf4d7aa587953c2bb1406a9710d14f3e4f83d
Jing Xiao <notajingoist@gmail.com>
2013-09-14 15:42:04 -0400

    new working template in example.js

---
+++ js/example.js
@@ -0,0 +1,31 @@
+window.IMPREGG || (IMPREGG = {}); //define a namespace
+
+(function($, window, document, undefined) {
+  
+    var EXAMPLE = IMPREGG.EXAMPLE = { //this is your js object
+        init: function() {
+            this.setVars();
+            this.bindEvents();
+        },
+
+        setVars: function() {
+            this.exampleString = "example";
+        },
+
+        bindEvents: function() {
+            // set event listeners for your functions here
+            $(document).on('click', function() {
+                console.log('example!');
+            });
+        },
+
+        // the rest of your functions go here
+        example: function () {
+
+        }
+    }
+
+    //uncomment this to test;
+    EXAMPLE.init();
+
+})(jQuery, window, document);
---
+++ js/impregg.js
@@ -0,0 +1,14 @@
+window.IMPREGG || (IMPREGG = {}); //define a namespace
+
+(function($, window, document, undefined) {
+    
+    IMPREGG.init = function() {
+        this.EXAMPLE.init();
+
+        //uncomment when we want to initialize these forreal
+        //this.PAN.init(); 
+    }
+
+    IMPREGG.init(); //initialize the app
+
+})(jQuery, window, document);
