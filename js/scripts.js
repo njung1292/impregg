e514ed1cb9e2598355118a1a4c49504f46d023e5
njjung <njjung@andrew.cmu.edu>
2013-09-14 15:23:01 -0400

    Working on the pan

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,17 @@ path.lineTo(start + [ 100, -50 ]);
 
 // Create a circle shaped path with its center at the center
 // of the view and a radius of 30:
-var path = new Path.Circle({
+var circle = new Path.Circle({
 	center: view.center,
 	radius: 250,
 	strokeColor: 'black'
 });
 
-//var path = new Path.Circle(view.bounds.center, 30);
-path.fillColor = 'red';
+//var circle = new Path.Circle(view.bounds.center, 30);
+circle.fillColor = 'red';
 
 function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	path.position = view.center;
+	// Whenever the window is resized, recenter the circle:
+	circle.position = view.center;
 }
 
