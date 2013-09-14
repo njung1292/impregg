2142f85f5ba060b3aff929cda0b02b8b8e0b5937
Jing Xiao <notajingoist@gmail.com>
2013-09-14 16:06:01 -0400

    paperscript mycanvas thingss

--- js/scripts.js
+++ js/scripts.js
@@ -26,11 +26,11 @@
 
 // Create a circle shaped path with its center at the center
 // of the view and a radius of 30:
-var circle = new Path.Circle({
-	center: view.center,
-	radius: 250,
-	strokeColor: 'black'
-});
+// var circle = new Path.Circle({
+// 	center: view.center,
+// 	radius: 250,
+// 	strokeColor: 'black'
+// });
 
 // //var path = new Path.Circle(view.bounds.center, 30);
 // path.fillColor = 'red';
